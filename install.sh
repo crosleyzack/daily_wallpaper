4a9fcd31af91f3b04cbf09783db007f904812997
crosleyzack <crosleyzack@gmail.com>
2023-05-02 21:29:42 -0400

    now working on startup

--- install.sh
+++ install.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-sudo apt install shuf \
-    imagemagick
+sudo apt install imagemagick
+
+chmod 777 ./set_wallpaper.sh
+chmod 777 $HOME/Pictures/Wallpapers
+chmod 666 quotes.txt
+chmod 777 wallpapers
+find wallpapers -type f -exec chmod 666 {} \;
+
+# Dumb permissions stuff
+sudo chown $USER:$USER ~/.config/dconf -R
+chmod u+w ~/.config/dconf -R
+
+
--- set_wallpaper.sh
+++ set_wallpaper.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
-WALLPAPER="$HOME/Pictures/wallpaper.png"
-TEXT=$(shuf -n 1 quotes.txt | xargs -n9)
-files=(./wallpapers/*)
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# https://askubuntu.com/questions/742870/background-not-changing-using-gsettings-from-cron
+REAL_UID=$(id --real --user)
+PID=$(pgrep --euid $REAL_UID gnome-session | head -n 1)
+export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2- | sed -e "s/\x0//g")
+
+# Set path to wallpaper
+WALLPAPER="$SCRIPT_DIR/wallpaper.png"
+
+# Remove old wallpaper
+rm -f $WALLPAPER
+gsettings set org.gnome.desktop.background picture-uri ""
+gsettings set org.gnome.desktop.background picture-uri-dark ""
+
+# Create new background
+TEXT=$(shuf -n 1 $SCRIPT_DIR/quotes.txt | xargs -n9)
+files=($SCRIPT_DIR/wallpapers/*)
 randomfile=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
+logger "Setting wallpaper $randomfile with text $TEXT"
 echo "wallpaper = $randomfile, TEXT = $TEXT"
-# randomfile="file://$HOME/dev/dotfiles/wallpaper/epictetus.jpg"
 convert "$randomfile" -pointsize 70 -fill white -gravity North -annotate +0+100 "$TEXT" -quality 100 "$WALLPAPER"
-# -font "Source Code Pro"
-gsettings set org.gnome.desktop.background picture-uri "$WALLPAPER"
-gsettings set org.gnome.desktop.background picture-uri-dark "$WALLPAPER"
+
+# Update background
+gsettings set org.gnome.desktop.background picture-uri "file://$WALLPAPER"
+gsettings set org.gnome.desktop.background picture-uri-dark "file://$WALLPAPER"
 gsettings set org.gnome.desktop.background picture-options "spanned"
 gsettings set org.gnome.desktop.background show-desktop-icons "true"
-# xwallpaper --stretch $randomfile
+logger "set_wallpaper.sh completed"
