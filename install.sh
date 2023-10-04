36a61e353b0abb5b711085f553429cbc7efe6984
crosleyzack <crosleyzack@gmail.com>
2023-10-04 12:20:46 -0400

    point install to newer quotes.json

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ chmod 777 ./create_wallpaper.sh
 chmod 777 ./sync_wallpaper.sh
 chmod 777 ./server.sh
 chmod 777 ./wallpaper_cron.sh
-chmod 666 quotes.txt
+chmod 666 quotes.json
 chmod 777 wallpapers
 find wallpapers -type f -exec chmod 666 {} \;
 
