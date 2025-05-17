01391b0b1a02312e6ba8a1fb18c5d421c48d4fd6
crosleyzack <crosleyzack@gmail.com>
2025-05-16 20:04:25 -0400

    sync uses wget

--- src/sync_wallpaper.sh
+++ src/sync_wallpaper.sh
@@ -2,8 +2,6 @@
 
 # Sync pulls the wallpaper from github where a daily workflow will generate it
 
-DEFAULT_WALLPAPER_NAME="wallpaper.png"
-
 # Ensure a base directory was provided.
 if [ ! -d "$1" ]
 then
@@ -13,12 +11,15 @@ fi
 if [ -z "$2" ]
 then
     # We will assume wallpaper name if not provided
-    WALLPAPER_NAME="$DEFAULT_WALLPAPER_NAME"
+    WALLPAPER_NAME="wallpaper.png"
 else
     WALLPAPER_NAME="$2"
 fi
 
 ASSETS_DIR=$(realpath $1)
 
+echo "sync_wallpaper.sh retrieving $ASSETS_DIR/$WALLPAPER_NAME"
 rm -f $ASSETS_DIR/$WALLPAPER_NAME
-curl https://raw.githubusercontent.com/CrosleyZack/random_desktop_quote/refs/heads/main/assets/wallpaper.png -o $ASSETS_DIR/$WALLPAPER_NAME
+wget -v -O $ASSETS_DIR/$WALLPAPER_NAME http://raw.githubusercontent.com/CrosleyZack/random_desktop_quote/refs/heads/main/assets/wallpaper.png
+# curl -v http://raw.githubusercontent.com/CrosleyZack/random_desktop_quote/refs/heads/main/assets/wallpaper.png -o $ASSETS_DIR/$WALLPAPER_NAME
+logger "sync_wallpaper.sh $ASSETS_DIR/$WALLPAPER_NAME retrieved"
